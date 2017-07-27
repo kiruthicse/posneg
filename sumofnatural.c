0b8defdcc157dc2f4a14a2ccc8185bee87c687eb
kiruthicse <30487214+kiruthicse@users.noreply.github.com>
2017-07-27 11:02:51 +0530

    Create sumofnatural.c

---
+++ sumofnatural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int s, i, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d",&s);
+    for(i=1; i <= n; ++i)
+    {
+        sum += i;
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
